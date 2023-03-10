7210911b67e19b29580b3d4c3e503a280c4cdf6c
Atharva-3000 <deshmukhsambhajirao@gmail.com>
2023-03-10 21:29:14 +0530

    uploading

---
+++ src/components/api.jsx
@@ -0,0 +1,13 @@
+const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'eed564ffccmsh0d16e9e8c81f5e3p13d31ajsnb10edd5e40a2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "85246861cf6da76fa7679dc5269986e9";
+export default geoApiOptions;
+
